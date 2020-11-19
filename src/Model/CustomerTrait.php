3c2e934989ee4d175288b97ccbcc78a4d9a85733
bartekTw <bartosz.twardowski@opoczta.pl>
2020-11-19 08:28:04 +0100

    customer entity and grid customization

--- src/Model/CustomerTrait.php
+++ src/Model/CustomerTrait.php
@@ -5,15 +5,9 @@ declare(strict_types=1);
 namespace BitBag\SyliusBlacklistPlugin\Model;
 
 use BitBag\SyliusBlacklistPlugin\Entity\Customer\CustomerInterface;
-use Doctrine\ORM\Mapping as ORM;
 
 trait CustomerTrait
 {
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="string", name="fraud_status")
-     */
     private $fraudStatus = CustomerInterface::FRAUD_STATUS_NEUTRAL;
 
     public function getFraudStatus(): string
