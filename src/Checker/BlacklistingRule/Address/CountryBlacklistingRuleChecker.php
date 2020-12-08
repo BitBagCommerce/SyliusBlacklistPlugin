1289bf8de244b025769fa645756cff27a200b13b
bartekTw <bartosz.twardowski@opoczta.pl>
2020-12-08 08:52:47 +0100

    fix content of BitBag comment

--- src/Checker/BlacklistingRule/Address/CountryBlacklistingRuleChecker.php
+++ src/Checker/BlacklistingRule/Address/CountryBlacklistingRuleChecker.php
@@ -2,7 +2,7 @@
 
 /*
  * This file has been created by developers from BitBag.
- * Feel free to contact us once you face...start
+ * Feel free to contact us once you face any issues or want to start
  * You can find more information about us on https://bitbag.io and write us
  * an email on hello@bitbag.io.
  */
