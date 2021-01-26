4c5020badbf4c6a42aa4bd51ca3fd67306d61e83
bartekTw <bartosz.twardowski@opoczta.pl>
2021-01-26 09:01:05 +0100

    fixes after CR

--- tests/Behat/Context/Setup/CustomerContext.php
+++ tests/Behat/Context/Setup/CustomerContext.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 declare(strict_types=1);
 
 namespace Tests\BitBag\SyliusBlacklistPlugin\Behat\Context\Setup;
@@ -8,7 +15,7 @@ use Behat\Behat\Context\Context;
 use Doctrine\Persistence\ObjectManager;
 use Sylius\Component\Core\Repository\CustomerRepositoryInterface;
 
-class CustomerContext implements Context
+final class CustomerContext implements Context
 {
     /** @var CustomerRepositoryInterface */
     private $customerRepository;
