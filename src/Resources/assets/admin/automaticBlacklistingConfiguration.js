1bbae4ab27d527dc1db395aa52978dc5b573061c
dead-playmod <playmod.pl@gmail.com>
2022-08-17 13:31:03 +0200

    Add event trigger on type select

---
+++ src/Resources/assets/admin/automaticBlacklistingConfiguration.js
@@ -0,0 +1,16 @@
+const addRuleBtn = document.querySelector('[data-form-collection="add"]');
+
+const turnOnListener = () => {
+  if (addRuleBtn) {
+    addRuleBtn.addEventListener('click', () => {
+      setTimeout(() => {
+        const configurationTypeSelects = document.querySelectorAll('[data-form-collection="update"]');
+        const changeEvent = new Event('change', { 'bubbles': true });
+    
+        configurationTypeSelects[configurationTypeSelects.length - 1].dispatchEvent(changeEvent);
+      }, 100);
+    });
+  }
+}
+
+turnOnListener();
