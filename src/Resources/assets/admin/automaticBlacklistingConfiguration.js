85519f2dd57f6f50e2c8c8bfda226000c455f592
dead-playmod <playmod.pl@gmail.com>
2022-08-19 11:14:58 +0200

    Webpack encore version bump

--- src/Resources/assets/admin/automaticBlacklistingConfiguration.js
+++ src/Resources/assets/admin/automaticBlacklistingConfiguration.js
@@ -1,16 +1,14 @@
 const addRuleBtn = document.querySelector('[data-form-collection="add"]');
 
 const turnOnListener = () => {
-  if (addRuleBtn) {
-    addRuleBtn.addEventListener('click', () => {
-      setTimeout(() => {
-        const configurationTypeSelects = document.querySelectorAll('[data-form-collection="update"]');
-        const changeEvent = new Event('change', { 'bubbles': true });
-    
-        configurationTypeSelects[configurationTypeSelects.length - 1].dispatchEvent(changeEvent);
-      }, 100);
-    });
-  }
+  addRuleBtn?.addEventListener('click', () => {
+    setTimeout(() => {
+      const configurationTypeSelects = document.querySelectorAll('[data-form-collection="update"]');
+      const changeEvent = new Event('change', { 'bubbles': true });
+  
+      configurationTypeSelects[configurationTypeSelects.length - 1].dispatchEvent(changeEvent);
+    }, 100);
+  });
 }
 
 turnOnListener();
