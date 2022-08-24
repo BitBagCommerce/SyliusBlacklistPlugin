13cc2f3b739e241466eb1e9ddafe937fb44ba6a0
liszkapawel <pawel.liszka00@gmail.com>
2022-08-24 10:22:14 +0200

    JS changes

--- src/Resources/assets/admin/automaticBlacklistingConfiguration.js
+++ src/Resources/assets/admin/automaticBlacklistingConfiguration.js
@@ -1,14 +1,15 @@
-const addRuleBtn = document.querySelector('[data-form-collection="add"]');
+$(() => {
+  const addRuleBtn = document.querySelector('[data-form-collection="add"]');
 
-const turnOnListener = () => {
-  addRuleBtn?.addEventListener('click', () => {
-    setTimeout(() => {
+  const turnOnListener = () => {
+    addRuleBtn?.addEventListener('click', () => {
       const configurationTypeSelects = document.querySelectorAll('[data-form-collection="update"]');
       const changeEvent = new Event('change', { 'bubbles': true });
-  
-      configurationTypeSelects[configurationTypeSelects.length - 1].dispatchEvent(changeEvent);
-    }, 100);
-  });
-}
 
-turnOnListener();
+      configurationTypeSelects[configurationTypeSelects.length - 1].dispatchEvent(changeEvent);
+    });
+  }
+
+  turnOnListener();
+})
+
