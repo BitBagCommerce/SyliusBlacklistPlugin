718faa29df48e29c171dde94313994503d1ab51c
janpalen <jan.palen@bitbag.pl>
2024-07-26 12:35:30 +0200

    OP-453 - Upgrade ecs

--- src/Entity/FraudPrevention/AutomaticBlacklistingConfiguration.php
+++ src/Entity/FraudPrevention/AutomaticBlacklistingConfiguration.php
@@ -1,10 +1,11 @@
 <?php
 
 /*
- * This file was created by developers working at BitBag
- * Do you need more information about us and what we do? Visit our https://bitbag.io website!
- * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
-*/
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
 
 declare(strict_types=1);
 
@@ -19,7 +20,6 @@ use Sylius\Component\Resource\Model\ToggleableTrait;
 class AutomaticBlacklistingConfiguration implements AutomaticBlacklistingConfigurationInterface
 {
     use ToggleableTrait;
-
     use TimestampableTrait;
 
     /** @var int|null */
@@ -30,14 +30,12 @@ class AutomaticBlacklistingConfiguration implements AutomaticBlacklistingConfigu
 
     /**
      * @var Collection|ChannelInterface[]
-     *
      * @psalm-var Collection<array-key, ChannelInterface>
      */
     protected $channels;
 
     /**
      * @var Collection|AutomaticBlacklistingRuleInterface[]
-     *
      * @psalm-var Collection<array-key, AutomaticBlacklistingRuleInterface>
      */
     protected $rules;
