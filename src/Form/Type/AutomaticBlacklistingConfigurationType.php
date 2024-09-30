f27a33f34e03c1c3104d9de6f747f8a9a0f115ba
jkindly <kozupa.jakub@gmail.com>
2024-09-30 14:10:30 +0200

    OP-558: init

--- src/Form/Type/AutomaticBlacklistingConfigurationType.php
+++ src/Form/Type/AutomaticBlacklistingConfigurationType.php
@@ -12,7 +12,9 @@ declare(strict_types=1);
 namespace BitBag\SyliusBlacklistPlugin\Form\Type;
 
 use BitBag\SyliusBlacklistPlugin\Entity\FraudPrevention\AutomaticBlacklistingRuleInterface;
+use Sylius\Bundle\AdminBundle\Form\Type\AddButtonType;
 use Sylius\Bundle\ChannelBundle\Form\Type\ChannelChoiceType;
+use Sylius\Bundle\PromotionBundle\Form\Type\PromotionRuleType;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -20,6 +22,7 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
+use Symfony\UX\LiveComponent\Form\Type\LiveCollectionType;
 
 final class AutomaticBlacklistingConfigurationType extends AbstractResourceType
 {
@@ -30,6 +33,7 @@ final class AutomaticBlacklistingConfigurationType extends AbstractResourceType
         string $dataClass,
         array $attributeChoices,
         array $validationGroups = [],
+        private array $rules,
     ) {
         parent::__construct($dataClass, $validationGroups);
         $this->attributeChoices = $attributeChoices;
@@ -72,9 +76,23 @@ final class AutomaticBlacklistingConfigurationType extends AbstractResourceType
                 ],
                 'placeholder' => 'bitbag_sylius_blacklist_plugin.ui.choose_time_range',
             ])
-            ->add('rules', AutomaticBlacklistingRuleCollectionType::class, [
-                'label' => 'bitbag_sylius_blacklist_plugin.form.automatic_blacklisting_rule.rules',
-                'button_add_label' => 'sylius.form.promotion.add_rule',
+//            ->add('rules', AutomaticBlacklistingRuleCollectionType::class, [
+//                'label' => 'bitbag_sylius_blacklist_plugin.form.automatic_blacklisting_rule.rules',
+//                'button_add_label' => 'sylius.form.promotion.add_rule',
+//            ])
+            ->add('rules', LiveCollectionType::class, [
+                'entry_type' => AutomaticBlacklistingRuleType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'button_add_type' => AddButtonType::class,
+                'button_add_options' => [
+                    'label' => 'sylius.ui.add_rule',
+                    'types' => $this->rules,
+                ],
+                'button_delete_options' => [
+                    'label' => false,
+                ],
             ])
         ;
     }
