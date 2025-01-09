010a1fac0bff6e13e31d0bfea18c3bc7f851f56c
Dariusz Rup <dariusz.rup@icloud.com>
2025-01-09 08:36:41 +0100

    OP-558 - change composer.json to sylius 2, update kernel and yaml files, fix bugs

--- tests/Application/Kernel.php
+++ tests/Application/Kernel.php
@@ -113,11 +113,8 @@ final class Kernel extends BaseKernel
     private function getConfigurationDirectories(): iterable
     {
         yield $this->getProjectDir() . '/config';
-        $syliusConfigDir = $this->getProjectDir() . '/config/sylius/' . SyliusCoreBundle::MAJOR_VERSION . '.' . SyliusCoreBundle::MINOR_VERSION;
-        if (is_dir($syliusConfigDir)) {
-            yield $syliusConfigDir;
-        }
-        $symfonyConfigDir = $this->getProjectDir() . '/config/symfony/' . BaseKernel::MAJOR_VERSION . '.' . BaseKernel::MINOR_VERSION;
+
+        $symfonyConfigDir = $this->getProjectDir() . '/config/symfony/' . BaseKernel::MAJOR_VERSION . 'x';
         if (is_dir($symfonyConfigDir)) {
             yield $symfonyConfigDir;
         }
